550046ecf1c12fbe4a9fb67f9e1541f9b6830dd8
shivshankar31 <shivshankar31@yahoo.com>
2021-11-28 05:23:38 +0530

    77: step 4: adding more field, setting validators, default value

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,9 +1,18 @@
-# In app models.py create class and field which creates DB structure, (Note: uniq identifier with auto increment is not requuired because it will be created automaticly)
+# step 1.3: In app models.py create class and field which creates DB structure, (Note: uniq identifier with auto increment is not requuired because it will be created automaticly)
+# step 4.1: In models.py, define __str__ with f string to view the book entries, to view the entries you have the quit the shell and reload.
+# step 4.2: now we have added new fileds author and is bestselling also set validator to rating by importing max and minlenghtvalidator, now run "makemigrations" and new file 0003 will be created. run "migrate" to add in the DB
 
 from django.db import models
-
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MaxLengthValidator(5), MinLengthValidator(1)])
+    author = models.CharField(null = True,max_length=50)
+    is_bestselling = models.BooleanField(default= False)
+
+
+
+    def __str__(self):
+        return f'{self.title},({self.rating})'
