aeae26ee34fc9692593cc214cacd699beb6ccf6f
shivshankar31 <shivshankar31@yahoo.com>
2021-11-29 05:19:46 +0530

    87: step 8: render index page and list all book

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,8 +1,13 @@
 # step 7.4: add index function in view.py file
+# step 8.1: In app, views.py import model and add sql quary to list all the book.
 
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 
 def index(request):
-    return render(request,'book_outlet/index.html')
+    all_book = Book.objects.all()
+    return render(request,'book_outlet/index.html', {
+        'books': all_book
+    })
