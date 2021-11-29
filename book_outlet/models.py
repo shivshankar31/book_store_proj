67977689ff380675a22c827912e5a2cf5ca60166
shivshankar31 <shivshankar31@yahoo.com>
2021-11-29 07:09:51 +0530

    89: step 10: add url to book title in index page

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,9 +1,13 @@
 # step 1.3: In app models.py create class and field which creates DB structure, (Note: uniq identifier with auto increment is not requuired because it will be created automaticly)
 # step 4.1: In models.py, define __str__ with f string to view the book entries, to view the entries you have the quit the shell and reload.
 # step 4.2: now we have added new fileds author and is bestselling also set validator to rating by importing max and minlenghtvalidator, now run "makemigrations" and new file 0003 will be created. run "migrate" to add in the DB
+# step 10.3: In models.py add get_absolute_url function and point url name
+
+
 
 from django.db import models
 from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.urls import reverse 
 # Create your models here.
 
 class Book(models.Model):
@@ -11,6 +15,11 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MaxLengthValidator(5), MinLengthValidator(1)])
     author = models.CharField(null = True,max_length=50)
     is_bestselling = models.BooleanField(default= False)
+
+
+    def get_absolute_url(self):
+        return reverse("details_book", args=[self.id])
+    
 
 
 
--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -1,6 +1,6 @@
 # step 7.5: create urls.py file inside app and add urlpatterns, path
 # step 9.2: In urls.py assign the path for the new function
-
+# tep 10.1: in urls.py add name to detail_book and also add id as int.
 
 from django.urls import path
 from .import views
@@ -8,5 +8,5 @@ from .import views
 urlpatterns = [
     
     path('', views.index, name='index'),
-    path('<id>', views.detail_book)
+    path('<int:id>', views.detail_book, name="details_book")
 ]
