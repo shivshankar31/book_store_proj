0602c108ca71bf6c35fb797103834b0d73297749
shivshankar31 <shivshankar31@yahoo.com>
2021-11-29 13:25:32 +0530

    90, 91: step 11,12: add slug field, modify save

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -2,12 +2,17 @@
 # step 4.1: In models.py, define __str__ with f string to view the book entries, to view the entries you have the quit the shell and reload.
 # step 4.2: now we have added new fileds author and is bestselling also set validator to rating by importing max and minlenghtvalidator, now run "makemigrations" and new file 0003 will be created. run "migrate" to add in the DB
 # step 10.3: In models.py add get_absolute_url function and point url name
-
+# step 11.1: add slug field to the class Book in modules.py
+# step 11.2: modify default save module, add Super().save() in order to recall the orginal save method.
+# step 11.3: open shell "python3 manage.py shell" and run below get() and call save()
+# step 12.3: change get_absolute_url function args to self.slug
+# step 12.4: add "db_index = True" to the slug field, it will increases the performance. index can be used where you use it more just to increase the performance.
 
 
 from django.db import models
 from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.urls import reverse 
+from django.utils.text import slugify
 # Create your models here.
 
 class Book(models.Model):
@@ -15,13 +20,15 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MaxLengthValidator(5), MinLengthValidator(1)])
     author = models.CharField(null = True,max_length=50)
     is_bestselling = models.BooleanField(default= False)
-
+    slug = models.SlugField(default="", null= False, db_index=True)
 
     def get_absolute_url(self):
-        return reverse("details_book", args=[self.id])
+        return reverse("details_book", args=[self.slug])
+
     
-
-
-
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, *kwargs)
+    
     def __str__(self):
         return f'{self.title},({self.rating}), {self.author},{self.is_bestselling}'
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,7 @@
 # step 7.4: add index function in view.py file
 # step 8.1: In app, views.py import model and add sql quary to list all the book.
 # step 9.2: In app/view.py create function and return the value
+# step 12.2: convert argument id to slug and compare slug = slug 
 
 
 from django.http.response import Http404
@@ -16,12 +17,12 @@ def index(request):
         'books': all_book
     })
 
-def detail_book(request, id):
+def detail_book(request, slug):
     # try:
     #     book = Book.objects.get(id = id)
     # except: 
     #     raise Http404
-    book = get_object_or_404(Book, pk =id)
+    book = get_object_or_404(Book, slug = slug)
     return render(request, 'book_outlet/detail_book.html',{
         'title' : book.title,
         'author' : book.author,
