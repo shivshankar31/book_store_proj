f5f5b85349a56c45c5302efc7db42a0c450e4ac6
shivshankar31 <shivshankar31@yahoo.com>
2021-11-29 06:18:03 +0530

    88: working on detail_book.html page, create function in view

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -1,9 +1,12 @@
 # step 7.5: create urls.py file inside app and add urlpatterns, path
+# step 9.2: In urls.py assign the path for the new function
+
 
 from django.urls import path
 from .import views
 
 urlpatterns = [
     
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<id>', views.detail_book)
 ]
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,7 +1,11 @@
 # step 7.4: add index function in view.py file
 # step 8.1: In app, views.py import model and add sql quary to list all the book.
+# step 9.2: In app/view.py create function and return the value
 
-from django.shortcuts import render
+
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls.conf import include
 from .models import Book
 
 # Create your views here.
@@ -11,3 +15,16 @@ def index(request):
     return render(request,'book_outlet/index.html', {
         'books': all_book
     })
+
+def detail_book(request, id):
+    # try:
+    #     book = Book.objects.get(id = id)
+    # except: 
+    #     raise Http404
+    book = get_object_or_404(Book, pk =id)
+    return render(request, 'book_outlet/detail_book.html',{
+        'title' : book.title,
+        'author' : book.author,
+        'rating' : book.rating,
+        'is_bestselling': book.is_bestselling 
+    }) 
